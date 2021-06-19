1f28933aea10119ae3f8abe082e135e58fd1d8bb
mokendkomer <me@mukundkumar.com>
2021-06-19 19:46:25 +0530

    added Mais' bot functions

---
+++ mutes.js
@@ -0,0 +1,54 @@
+const config = require('../config.json')
+const Discord = require("discord.js");
+const ms = require("ms");
+const client = new Discord.Client();
+
+client.on('message', message => {
+    if(message.content.toLowerCase().startsWith('kat mute')){
+        const args = message.content.slice(4).split(/ +/);
+        const misperms = `⚠️ <@${message.author.id}> You cannot run this command.`
+        const target = message.mentions.users.first();
+        if (!message.member.hasPermission(["MANAGE_ROLES"])) return message.channel.send(misperms);
+        if (target){
+            let muteRole = message.guild.roles.cache.find(role => role.name === "Muted");
+            let memberTarget = message.guild.members.cache.get(target.id);
+            if(!args[2]){
+                memberTarget.roles.add(muteRole.id);
+                message.channel.send(`<@${memberTarget.user.id}> has been muted.`);
+                return
+            }
+            memberTarget.roles.add(muteRole.id);
+            message.channel.send(`<@${memberTarget.user.id}> has been muted for ${ms(args[2])}.`);
+            
+            setTimeout(function(){
+                memberTarget.roles.remove(muteRole.id);
+                message.channel.send(`<@${memberTarget.user.id}> has been unmuted.`);
+            }, ms(args[2]));
+        } else{
+            message.channel.send('Sorry, cannot find the member.')
+        }        
+    }
+    if(message.content.toLowerCase().startsWith('kat unmute')){
+        const target = message.mentions.users.first();
+        const misperms = `⚠️ <@${message.author.id}> You seem to be missing the permissions to run this command.`
+
+        if (!message.member.hasPermission(["MANAGE_ROLES"])) return message.channel.send(misperms);
+        
+        if (target){
+            let muteRole = message.guild.roles.cache.find(role => role.name === "Muted");
+
+            let memberTarget = message.guild.members.cache.get(target.id);
+
+            memberTarget.roles.remove(muteRole.id);
+            message.channel.send(`<@${memberTarget.user.id}> has been unmuted.`);
+
+        } else{
+            message.channel.send('Sorry, cannot find the member.')
+        }
+
+    }
+})
+
+
+
+client.login(config.katToken);
---
+++ purge.js
@@ -0,0 +1,20 @@
+const config = require('../config.json')
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on('message', async message => {
+    if(message.content.toLowerCase().startsWith('kat purge')){
+        const misperms = `⚠️ <@${message.author.id}> You seem to be missing the permissions to run this command.`
+        if (!message.member.hasPermission(["MANAGE_MESSAGES"])) return message.channel.send(misperms);
+        const args = message.content.slice(4).split(/ +/);
+        if (!args[1]) return message.channel.send(`⚠️ <@${message.author.id}> Please enter the amount of messages you want to purge.`);
+        if (isNaN(args[1])) return message.channel.send(`⚠️ <@${message.author.id}> Please enter a real number.`);
+        if (args[1] > 100) return message.channel.send(`⚠️ <@${message.author.id}> You cannot delete more than 100 messages.`);
+        if (args[1] < 1) return message.channel.send(`⚠️ <@${message.author.id}> You must delete at least one message.`);
+        await message.channel.messages.fetch({ limit: ++args[1] }).then(messages => {
+            message.channel.bulkDelete(messages);
+        });
+    }
+})
+
+client.login(config.katToken);
