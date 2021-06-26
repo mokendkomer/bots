ad4cc3b06535aff7d2b008bfebbd47dcbb68b332
mokendkomer <47618977+mokendkomer@users.noreply.github.com>
2021-06-26 16:07:23 +0530

    added checkin and reminders bots

---
+++ checkin.js
@@ -0,0 +1,92 @@
+const config = require('../config.json');
+const Discord = require("discord.js");
+const cron = require('node-cron');
+const fs = require("fs");
+const client = new Discord.Client({
+	partials: ['MESSAGE', 'REACTION']
+});
+const emotes = ['<:greenheart:858286732314017803>', '<:blueheart:858286731908743191>', '<:purpleheart:858286732410617877>', '<:whiteheart:858286732049776681>', '<:yellowheart:858286732180455475>', '<:orangeheart:858286732382306324>', '<:blackheart:858284240566091786>', '<:brokenheart:858286731907432460>']
+const channelID = ''
+let file = require('../checkin.json')
+const characters = file.characters
+let character = file.character
+const read = () => {
+	file = require('../checkin.json')
+	character = file.character
+}
+const write = () => {
+	file.character = character
+	fs.writeFileSync('../checkin.json', JSON.stringify(file))
+}
+const pickCharacter = () => {
+	character = characters[Math.floor(Math.random()*characters.length)];
+	write()
+	const webhookClient = new Discord.WebhookClient(config.notCheckinWebhook.id, config.notCheckinWebhook.token)
+    webhookClient.send(`I will do the checkin today. Be sure to set an image I can use for checkin using the '.setimage' command`, {
+		username: character.name,
+		avatarURL: character.avatar,
+		image: ''
+	  })
+}
+
+const sendPrompt = () => {
+	read()
+	const embed = new Discord.MessageEmbed()
+	embed.setColor('5af27d')
+	embed.setTitle(`How are you feeling today? 🌈`)
+	embed.setDescription(`<:greenheart:858286732314017803> - Amazing\n<:blueheart:858286731908743191> - Good\n<:purpleheart:858286732410617877> - Fine/Okay/Neutral\n<:whiteheart:858286732049776681> - I don't know how I'm feeling right now\n<:yellowheart:858286732180455475> - I think I will be fine\n<:orangeheart:858286732382306324> - I am struggling right now\n<:blackheart:858284240566091786> - I am having a really hard time and need someone to talk to\n<:brokenheart:858286731907432460> - I am at my lowest, and in a really dark place right now`)
+	if(character.image)
+		embed.setImage(character.image)
+	const webhookClient = new Discord.WebhookClient(config.checkinWebhook.id, config.checkinWebhook.token)
+	webhookClient.send({
+		username: character.name,
+		avatarURL: character.avatar,
+		embeds: [embed]
+	})
+
+}
+
+client.on('message', message => {
+	if(message.channel.id === "852563682419540069" && message.webhookID)
+		emotes.forEach(async emote => await message.react(emote))
+
+	if(message.content.toLowerCase().startsWith('.pickcharacter'))
+		pickCharacter()
+	if(message.content.toLowerCase().startsWith('.sendprompt'))
+		sendPrompt()
+	if(message.content.toLowerCase().startsWith('.setimage')){
+		if(message.attachments.first()){
+			character.image = message.attachments.first().url
+		} else if(message.content.length > 10)
+			character.image = message.content.substring(10)
+		else return message.channel.send(`I couldn't find the image you want to set.`)
+		write()
+		message.channel.send(`Thank you for setting today's image.`)
+	}
+})
+
+client.on('messageReactionAdd', async (reaction, user) => {
+	if (user.bot)
+		return;
+	if (reaction.partial) {
+		try {
+			await reaction.fetch();
+		} catch (error) {
+			console.log('Something went wrong when fetching the message: ', error);
+			return;
+		}
+	}
+
+	if (reaction.message.channel.id === '852563682419540069' && !emotes.includes(reaction.emoji.name))
+		return reaction.remove();
+})
+
+cron.schedule('0 15 * * *', async () => {
+	pickCharacter()	  
+});
+
+cron.schedule('0 17 * * *', async () => {
+	sendPrompt()
+})
+
+client.login(config.katToken)
---
+++ reminders.js
@@ -0,0 +1,118 @@
+const config = require('../config.json')
+const Discord = require("discord.js");
+const fs = require('fs');
+const cron = require('node-cron')
+const client = new Discord.Client();
+const mokendDm = '845575192688459786';
+const srijaDm = '845575380970504224';
+let reminders = [];
+let embed = {}
+const write = () => fs.writeFileSync('../reminders.json', JSON.stringify(reminders));
+const read = () => reminders = JSON.parse(fs.readFileSync('../reminders.json'));
+const makeEmbed = () => {
+	embed = new Discord.MessageEmbed()
+	embed.setColor('f2a0eb')
+}
+let text = ``
+const wittleTolk = (thing) => {
+	text = thing
+	text = text.replace('r', 'w')
+	text = text.replace('l', 'w')
+	text = text.replace('my', 'moi')
+}
+const { uniqueNamesGenerator, adjectives, colors, animals } = require('unique-names-generator');
+
+const takeReminder = (activity, user) => {
+	read()
+	const id = uniqueNamesGenerator({ dictionaries: [adjectives, colors, animals] });
+	reminders.push({
+		id,
+		user: user,
+		activity: activity
+	});
+	write()
+	return id;
+}
+
+const markDone = (id) => {
+	read()
+	let task = reminders.find(reminder => reminder.id === id);
+	if (!task)
+		return `o nno! I can't find ${id}`
+	reminders = reminders.filter(item => item.id !== id);
+	write()
+	return 'yayayyyyayayy I gwad to heow that you aw dono wif ' + task.activity + " heow is a cookie 🍪"
+}
+const remind = (array) => {
+	array.forEach(reminder => {
+		makeEmbed()
+		embed.setTitle(`🍪 wemindoww 🍪`)
+		embed.setDescription(`I hope you wemembow to do this\nif you don't, that is okay, I'm heow to wemind you nowie\ncan you pwease do this:\n> **${reminder.activity}**`)
+		embed.setFooter(reminder.id)
+		client.users.cache.get(reminder.user).send(embed)
+	})
+
+}
+
+client.on('message', message => {
+	if (message.content.toLowerCase().startsWith('remind me to ')) {
+		if (message.content.length < 13)
+			return message.channel.send('o nno! you need to mention what you want to be weminded about\ncan you twi againn')
+		wittleTolk(message.content.substring(13))
+		const id = takeReminder(text, message.author.id)
+		makeEmbed()
+		embed.setTitle(`new wemindoww`)
+		embed.setDescription(`I will wemind you to ${text}`)
+		embed.setFooter(id)
+		message.channel.send(embed)
+	} else if (message.content.toLowerCase().startsWith('remind him to ')) {
+		if (message.content.length < 14)
+			return message.channel.send('o nno! you need to mention what you want him to be weminded about\ncan you twi againn')
+		wittleTolk(message.content.substring(14))
+		takeReminder(text, mokendDm)
+		message.channel.send(`okkii I remind him to ${text} evowy 2 howows`)
+	} else if (message.content.toLowerCase().startsWith("i'm done with ")) {
+		message.channel.send(markDone(message.content.substring(14)))
+	} else if (message.content.toLowerCase().startsWith("im done with ")) {
+		message.channel.send(markDone(message.content.substring(13)))
+		} else if (message.content.toLowerCase().startsWith('what are my reminders')){
+			read()
+			const partialReminders = reminders.filter(reminder => reminder.user === message.author.id)
+			remind(partialReminders)
+			
+
+	}
+})
+
+client.on('ready', () => {
+	// client.users.cache.get(srijaDm).send('HEWWOO I hope you aw happy I wuvv youuu')
+	console.log('I am weady to twi mi bestt big pwomise');
+})
+
+cron.schedule('0 9-23/2 * * *', () => {
+	read()
+	remind()
+})
+
+cron.schedule(
+	'0 9 * * *',
+	() => {
+		client.user.setActivity('with wittol mokendd ', { type: 'PLAYING' });
+	}, {
+	scheduled: true,
+	timezone: 'Asia/Kolkata'
+}
+);
+
+cron.schedule(
+	'0 21 * * *',
+	() => {
+		client.user.setActivity('in moi dweams ', { type: 'PLAYING' });
+	}, {
+	scheduled: true,
+	timezone: 'Asia/Kolkata'
+}
+);
+
+
+client.login(config.littleSrijaToken);
