20a5eb49ec5a3ebda9d661583dab41bd7ced79b3
mokendkomer <47618977+mokendkomer@users.noreply.github.com>
2021-07-01 00:53:05 +0530

    Update quocommbot.js

--- quocommbot.js
+++ quocommbot.js
@@ -1,4 +1,4 @@
-const config = require('./config.json')
+const config = require('../config.json')
 const Discord = require("discord.js");
 const client = new Discord.Client({ partials: ['MESSAGE', 'REACTION'] });
 const pendingVerification = `587744410498760707`;
@@ -6,7 +6,7 @@ const verification = `587156836205985793`
 const member = `587187354851082250`
 const readme = `587156775346765834`
 const park = `587321519051636776`
-const verifiers =  require('./verifiers.json')
+const verifiers =  require('../verifiers.json')
 const findVerifier = (id) => {
 	return verifiers.find(person => person.id === id)
 }
