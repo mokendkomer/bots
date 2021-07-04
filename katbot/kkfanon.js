425c0f533fffa219ee1ae91386891810d533aa0d
Srija M <73830590+ajirs23@users.noreply.github.com>
2021-07-04 16:56:35 +0530

    Add anonymous feature for support chats

--- katbot/kkfanon.js
+++ katbot/kkfanon.js
@@ -8,7 +8,11 @@ client.on("message", (message) => {
 	  const args = message.content.toLowerCase().split(" ")
 	if(
 		args[1] !== "share" &&
-		args[1] !== "vent"
+		args[1] !== "vent" &&
+		args[1] !== "support" &&
+		args[1] !== "support1" &&
+		args[1] !== "support2" &&
+		args[1] !== "support3"
 	) return;
 	const member = client.guilds.cache.get('641530868267089920').member(message.author.id)
 	if(!member || !member.roles.cache.some(role => role.id === '642545338112016408'))
@@ -23,8 +27,28 @@ client.on("message", (message) => {
 		}
 		if(args[1].toLowerCase() === "vent"){
 		channel = "753504459837014035";
-		prompt = "Your message has been sent. Take care!";
+		prompt = "Your message has been sent. I hope you feel better soon. Take care!";
 		substring = 9;
+		}
+		if(args[1].toLowerCase() === "support"){
+			channel = "642037072890429451";
+			prompt = "Your message has been sent. I hope you feel better soon. Take care!";
+			substring = 12;
+		}
+		if(args[1].toLowerCase() === "support1"){
+			channel = "642037072890429451";
+			prompt = "Your message has been sent. I hope you feel better soon. Take care!";
+			substring = 13;
+		}
+		if(args[1].toLowerCase() === "support2"){
+			channel = "674618839060971550";
+			prompt = "Your message has been sent. I hope you feel better soon. Take care!";
+			substring = 14;
+		}
+		if(args[1].toLowerCase() === "support3"){
+			channel = "698099638850289685";
+			prompt = "Your message has been sent. I hope you feel better soon. Take care!";
+			substring = 15;
 		}
 		if(message.content.includes('@everyone') || message.content.includes('@here') || message.content.includes('<@'))
 		    return message.channel.send(`Your message is not allowed to ping anyone.`)
