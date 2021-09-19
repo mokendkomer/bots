96d53bffaea79c482d97a8738b57e8c32c769213
mokendkomer <47618977+mokendkomer@users.noreply.github.com>
2021-09-20 00:50:15 +0530

    Update favoritepart.js

--- cute/favoritepart.js
+++ cute/favoritepart.js
@@ -16,12 +16,12 @@ const srijabear = "<:srijalove:793852415228903474>";
 let mokendfav = `Probably that time when he ignored my DMs when I asked what his favorite part was ${emoji}`;
 let srijafav = `Probably that time when she ignored my DMs when I asked what her favorite part was ${emoji}`;
 let mokendprod = {
-	activities: "nono he was busy being a wastemokend",
+	activities: "notdone",
 	satisfied: "",
 	reason: "",
 };
 let srijaprod = {
-	activities: "nono she was busy being a wastesrija",
+	activities: "notdone",
 	satisfied: "",
 	reason: "",
 };
@@ -31,7 +31,11 @@ cron.schedule(
 		client.users.fetch(mokendDm).then((mokend) => mokend.send("Hewwo mokend mokend! What pwoductive things did you do today"));
 		client.users.fetch(srijaDm).then((srija) => srija.send("Oioi srija srija! What pwoductive things did you do today?"));
 		mokendprod.activities = "nono he was busy being a wastemokend";
+		mokendprod.satisfied = "";
+		mokendprod.reason = "";
 		srijaprod.activities = "nono she was busy being a wastesrija";
+		srijaprod.satisfied = "";
+		srijaprod.reason = "";
 	},
 	{
 		scheduled: true,
@@ -50,15 +54,17 @@ cron.schedule(
 			.setColor("#0a9ba6")
 			.setAuthor("Mokendbear", client.users.cache.get(mokendDm).avatarURL())
 			.setTitle(`Mukund's productivity for today! ${mokendbear}`)
-			.setDescription(`**Things done**\n${mokendprod.activities}\n\n Mokend is ${mokendprod.satisfied.startsWith("y") ? "happy" : "not happy"} with his productivity today ${mokendprod.satisfied.startsWith("y") ? "" : "becasue " + mokendprod.reason}`);
+			.setDescription(`**Things done**\n${mokendprod.activities}${mokendprod.activities.startsWith("nono") ? `` : `\n\n Mokend is ${mokendprod.satisfied.startsWith("y") ? "happy" : "not happy"} with his productivity today ${mokendprod.satisfied.startsWith("y") ? "" : "because " + mokendprod.reason}`}`);
 		let srijaEmbed = new Discord.MessageEmbed()
 			.setColor("#0a9ba6")
 			.setAuthor("Srijabear", client.users.cache.get(srijaDm).avatarURL())
 			.setTitle(`Srija's productivity for today!  ${srijabear}`)
-			.setDescription(`**Things done**\n${srijaprod.activities}\n\n Srija is ${srijaprod.satisfied.startsWith("y") ? "happy" : "not happy"} with her productivity today ${srijaprod.satisfied.startsWith("y") ? "" : "becasue " + srijaprod.reason}`);
+			.setDescription(`**Things done**\n${srijaprod.activities}${srijaprod.activities.startsWith("nono") ? `` : `\n\n Srija is ${srijaprod.satisfied.startsWith("y") ? "happy" : "not happy"} with her productivity today ${srijaprod.satisfied.startsWith("y") ? "" : "because " + srijaprod.reason}`}`);
 
 		client.channels.cache.get(prodchannel).send(mokendEmbed);
 		client.channels.cache.get(prodchannel).send(srijaEmbed);
+		mokendprod.activities = "notdone";
+		srijaprod.activities = "notdone";
 	},
 	{
 		scheduled: true,
@@ -91,46 +97,50 @@ process.on("unhandledRejection", (error) => {
 client.on("message", async (message) => {
 	if (message.channel.type === "dm" && !message.author.bot) {
 		if (message.author.id === mokendDm) {
-			if (mokendprod.activities.startsWith("nono")) {
-				mokendprod.activities = message.content;
-				message.channel.send("oki okiii, awe you happy wif how pwoductive you woww?");
-			} else if (mokendprod.satisfied === "") {
-				mokendprod.satisfied = message.content;
-				if (message.content.startsWith("n")) message.channel.send("owwiee oki, and is theow any weason fow that");
-				else message.channel.send("okii fank woo fow hewping wif yow answows todayy MWAH :cookie:");
-			} else if (mokendprod.satisfied.startsWith("n") && mokendprod.reason === "") {
-				mokendprod.reason = message.content;
-				message.channel.send("fank woo fow hewping me wif yow answows todayyy MWAHH :cookie:");
-			} else {
+			if (mokendprod.activities === "notdone") {
 				mokendfav = message.content;
 				if (message.content.length > 2048) {
 					message.channel.send(`Oh no! This is tooo big s m h. Twiii sending something that I can wemembewww hmpff ${emoji}`);
 					mokendfav = `He sent something that's too big for me to wemembewwww ${emoji}`;
 					return;
 				} else {
-					message.channel.send(`Yayaayay! I can send this thing when it's the right time hehehe!`);
+					return message.channel.send(`Yayaayay! I can send this thing when it's the right time hehehe!`);
 				}
+			} else if (mokendprod.activities.startsWith("nono")) {
+				mokendprod.activities = message.content;
+				message.channel.send("oki okiii, awe you happy wif how pwoductive you woww?");
+				return;
+			} else if (mokendprod.satisfied === "") {
+				mokendprod.satisfied = message.content;
+				if (message.content.startsWith("n")) return message.channel.send("owwiee oki, and is theow any weason fow that");
+				else return message.channel.send("okii fank woo fow hewping wif yow answows todayy MWAH :cookie:");
+			} else if (mokendprod.satisfied.startsWith("n") && mokendprod.reason === "") {
+				mokendprod.reason = message.content;
+				message.channel.send("fank woo fow hewping me wif yow answows todayyy MWAHH :cookie:");
+				return;
 			}
 		} else if (message.author.id === srijaDm) {
-			if (srijaprod.activities.startsWith("nono")) {
-				srijaprod.activities = message.content;
-				message.channel.send("oki okiii, awe you happy wif how pwoductive you woww?");
-			} else if (srijaprod.satisfied === "") {
-				srijaprod.satisfied = message.content;
-				if (message.content.startsWith("n")) message.channel.send("owwiee oki, and is theow any weason fow that");
-				else message.channel.send("okii fank woo fow hewping wif yow answows todayy MWAH :cookie:");
-			} else if (srijaprod.satisfied.startsWith("n") && srijaprod.reason === "") {
-				srijaprod.reason = message.content;
-				message.channel.send("fank woo fow hewping me wif yow answows todayyy MWAHH :cookie:");
-			} else {
+			if (srijaprod.activities === "notdone") {
 				srijafav = message.content;
 				if (message.content.length > 2048) {
 					message.channel.send(`Oh no! This is tooo big s m h. Twiii sending something that I can wemembewww hmpff ${emoji}`);
 					srijafav = `She sent something that's too big for me to wemembewwww ${emoji}`;
 					return;
 				} else {
-					message.channel.send(`Yayaayay! I can send this thing when it's the right time hehehe!`);
+					return message.channel.send(`Yayaayay! I can send this thing when it's the right time hehehe!`);
 				}
+			} else if (srijaprod.activities.startsWith("nono")) {
+				srijaprod.activities = message.content;
+				message.channel.send("oki okiii, awe you happy wif how pwoductive you woww?");
+				return;
+			} else if (srijaprod.satisfied === "") {
+				srijaprod.satisfied = message.content;
+				if (message.content.startsWith("n")) return message.channel.send("owwiee oki, and is theow any weason fow that");
+				else return message.channel.send("okii fank woo fow hewping wif yow answows todayy MWAH :cookie:");
+			} else if (srijaprod.satisfied.startsWith("n") && srijaprod.reason === "") {
+				srijaprod.reason = message.content;
+				message.channel.send("fank woo fow hewping me wif yow answows todayyy MWAHH :cookie:");
+				return;
 			}
 		}
 	}
